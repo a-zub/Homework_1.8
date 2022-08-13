2c915368478328a09bf0a77b18820ce80f7b9434
Sasha <slanecru@outlook.com>
2022-08-13 09:26:19 +0300

    corrected homework 1.8

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,11 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         calculateLeapYear(1917); // Task 1
-        printCurrentOS(1, 1977); // Task 2
+
+        int typeOS = 0; // Task 2
+        int yearOfIssue = 2002;
+        System.out.println(printCurrentOS(typeOS, yearOfIssue));
+
         int deliveryDistance = 200; // Task 3
         System.out.println("Потребуется дней: " + calculateDeliveryDays(deliveryDistance));
     }
@@ -18,34 +22,40 @@ public class Main {
 
     public static void calculateLeapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println((int) year + " год является високосным");
+            System.out.println((int) year + " год является високосным.");
         } else {
-            System.out.println((int) year + " год не является високосным");
+            System.out.println((int) year + " год не является високосным.");
         }
     }
 // Task 2
 
 
-    public static void printCurrentOS(int typeOS, int yearOfIssue) {
+    public static String printCurrentOS(int typeOS, int yearOfIssue) {
         int currentYear = LocalDate.now().getYear();
-        if (typeOS == 0 && yearOfIssue >=  currentYear) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (typeOS == 0 && yearOfIssue <  currentYear) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (typeOS == 1 && yearOfIssue >=  currentYear) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (typeOS == 1 && yearOfIssue <  currentYear) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Ваша версия операционной системы не найдена.");
+        String titleOS;
+        String OsVersion;
+        String inputError = "Вы ввели не верные данные.";
+        if (typeOS < 0 || typeOS > 1 || yearOfIssue < 0) {
+            return inputError;
         }
+        if (typeOS == 1) {
+            titleOS = "Андроид";
+        } else {
+            titleOS = "iOS";
+        }
+        if(yearOfIssue < currentYear){
+            OsVersion = "облегченную версию";
+        } else {
+            OsVersion = "стандартную";
+        }
+         return "Установите " + OsVersion + " версию приложения для " + titleOS + " по ссылке.";
     }
 // Task 3
 
     public static int calculateDeliveryDays(int deliveryDistance) {
-       int deliveryDays = 1;
-       int interval = 40;
-       int startInterval = 20;
+        int deliveryDays = 1;
+        int interval = 40;
+        int startInterval = 20;
         if (deliveryDistance > startInterval) {
             deliveryDays = deliveryDays + (int) Math.ceil((deliveryDistance - startInterval) / (double) interval);
         }
